89d76e6d79bc9d24b8b75704a607f63b36384a4d
heejaykong <heejay.dev@gmail.com>
2021-04-11 16:48:57 +0900

    func: create /post/:id router

--- Web-Front-End/src/App.js
+++ Web-Front-End/src/App.js
@@ -8,6 +8,39 @@ import Post from './routes/Post';
 import Reserve from './routes/Reserve';
 import Footer from './components/Footer';
 
+const posts = [
+  {
+    id: 1,
+    title: "앙!",
+    content: "어어아아아앙"
+  },
+  {
+    id: 2,
+    title: "애애애오옹!",
+    content: "야옹! 야옹! 야옹! 야옹! 야옹! 야옹! 야옹! 야옹! "
+  },
+  {
+    id: 3,
+    title: "월워러럴우렁루러!",
+    content: "멍! 멍! 멍! 멍! 멍! 멍! 멍! 멍! "
+  },
+  {
+    id: 4,
+    title: "앙!4",
+    content: "어어아아아앙"
+  },
+  {
+    id: 5,
+    title: "5애애애오옹!",
+    content: "야옹! 야옹! 야옹! 야옹! 야옹! 야옹! 야옹! 야옹! "
+  },
+  {
+    id: 6,
+    title: "6월워러럴우렁루러!",
+    content: "멍! 멍! 멍! 멍! 멍! 멍! 멍! 멍! "
+  },
+];
+
 function App() {
   return (
     <HashRouter>
@@ -15,7 +48,11 @@ function App() {
       <Route path="/" exact={true} component={Home}/>
       <Route path="/web" component={Web}/>
       <Route path="/club" component={Club}/>
-      <Route path="/post" component={Post}/>
+      {/* <Route path="/post" component={Post}/> */}
+      <Route path={"/post/:id"} render={ ({match}) => {
+        console.log(match);
+        return <Post post={ posts.find(p => p.id === parseInt(match.params.id)) } />
+      }} />
       <Route path="/reserve" component={Reserve}/>
       <Footer />
     </HashRouter>
--- Web-Front-End/src/routes/Post.js
+++ Web-Front-End/src/routes/Post.js
@@ -5,20 +5,22 @@ import Col from 'react-bootstrap/Col'
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
 
-function Post(){
+function Post({post}){
   return (
+    <>
       <Container style={{ marginTop: '2em' }}>
         <Card>
           <Card.Header>게시판이름</Card.Header>
           <Card.Body>
-            <Card.Title>이번 총회는 온라인입니다</Card.Title>
+            <Card.Title>{post.title}</Card.Title>
             <small className="text-muted">작성자: 공희재</small>
             <Card.Text>
-              코로나 때문에 이번 총회도 온라인으로 진행될 예정임당~ 신사임당~ 감사함당~
+              {post.content}
             </Card.Text>
           </Card.Body>
         </Card>
       </Container>
+    </>
   )
 }
 
